701f5b73dddd44a813bd503cc97b315ccda7ca2e
GRaghuNandanLal <53864248+GRaghuNandanLal@users.noreply.github.com>
2020-12-07 08:41:46 +0530

    Add files via upload

--- app/build/generated/source/buildConfig/debug/jetray/tictactoe/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/jetray/tictactoe/BuildConfig.java
@@ -1,1 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jetray.tictactoe;
 
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "jetray.tictactoe";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.2";
+}
